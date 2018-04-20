f719f4dd2d3bf949c7ae28e2280c010bf40a8fa8
Scott Davis <scott@thirstyhead.com>
2018-04-20 15:08:12 -0400

    creating setup.sh

---
+++ bower_components/talk-o-vision/setup.sh
@@ -0,0 +1,9 @@
+# setup.sh sets up a new slidedeck
+# To do so, it copies all necessary files
+# from bower_components to the root directory
+
+cp -r -i -v slides/ ../..
+cp -i -v talk-o-vision.* ../..
+cp -i -v index-slideshow.html ../../index.html
+cp -i -v notes.html ../..
+cp -i -v toc.html ../..
